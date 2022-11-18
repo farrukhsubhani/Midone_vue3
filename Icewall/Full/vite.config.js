386048cb7b41a4582e9eebc827d87f93757e9e68
Farrukh Subhani <farrukh@bookingonline.co.uk>
2022-11-18 17:33:11 +0000

    feat: :sparkles: Added Icewall

---
+++ Icewall/Full/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: path.resolve(__dirname, "../../Releases/icewall-full"),
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});

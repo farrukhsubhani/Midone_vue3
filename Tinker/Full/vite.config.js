8cd60ce18196d6e54dc42e6162c01dd651277cd0
Farrukh Subhani <farrukh@bookingonline.co.uk>
2022-11-18 17:45:25 +0000

    feat: :sparkles: Added Index page and release path

--- Tinker/Full/vite.config.js
+++ Tinker/Full/vite.config.js
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: path.resolve(__dirname, "../../Releases/tinker-full"),
+  },
   plugins: [vue()],
   resolve: {
     alias: {

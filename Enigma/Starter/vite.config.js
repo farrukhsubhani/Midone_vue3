190cb80304702bdad81e66e2d0e2a784a844c83c
Farrukh Subhani <farrukh@bookingonline.co.uk>
2022-11-18 17:23:40 +0000

    Added Release path to Enigma

--- Enigma/Starter/vite.config.js
+++ Enigma/Starter/vite.config.js
@@ -4,6 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: path.resolve(__dirname, "../../Releases/enigma-starter"),
+  },
   plugins: [vue()],
   resolve: {
     alias: {
